54e6c554f3e59760bb55d17d8d61c3ef1380f075
NahuelCerrato <nahuelcerrato@gmail.com>
2025-03-26 19:45:14 -0300

    Se creo segundo archivo

---
+++ Segundo.py
@@ -0,0 +1,4 @@
+nombre = input("Ingrese nombre: ")
+apellido = input("Ingrese apellido: ")
+carrera = "Tecnicatura Universitaria en Programación"
+print(f"Hola soy {apellido} {nombre} estudiante de la carrera {carrera}")
